b77d267ae816ab996491c27201c3cd04bb0f0be2
Sonechek <71063702+Sonechek@users.noreply.github.com>
2021-06-22 15:17:41 +0500

    AI

---
+++ main_tensor.py
@@ -0,0 +1,19 @@
+'''
+Пока что пусто, установка и отладка всех требуемых библиотек
+'''
+
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+print("TF version:", tf.__version__)
+print("Hub version:", hub.__version__)
+print("GPU is", "available" if tf.test.is_gpu_available() else "NOT AVAILABLE")
+'''
+Проверка на работоспособность GPU
+'''
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
